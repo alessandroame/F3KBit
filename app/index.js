52a7ba7e42c40ec680c16d2b673ee7b1d46ba6f1
aame <alessandroame@gmail.com>
2021-09-08 23:05:09 +0200

    calibration process

--- app/index.js
+++ app/index.js
@@ -10,15 +10,32 @@ launchDetector.start();
 
 let chart=new Chart("launch_chart");
 launchDetector.setChart(chart);
+document.getElementById("status").textContent="waiting for launch..";
 function onLaunchTriggered(){
   launchDetector.stop();
   vibration.start("nudge-max");
+  document.getElementById("status").textContent="launch triggered";
   console.log("launch triggered");
 }
 
+let leftBtn = document.getElementById("leftBtn");
+leftBtn.onclick=()=>{
+  launchDetector.calibrate((v)=>{
+    document.getElementById("status").textContent=v.toFixed(2);
+  },(v)=>{
+    document.getElementById("status").textContent="Done "+v.toFixed(2);
+    launchDetector.stop();
+  });
+}
+let rigthBtn = document.getElementById("rigthBtn");
+rigthBtn.onmousemove = function(evt) {
+  //simulateZAxis(evt);
+}
+rigthBtn.onclick=()=>{
+  launchDetector.toggle();
+}
 
-let controller = document.getElementById("controller");
-controller.onmousemove = function(evt) {
+function simulateZAxis(evt){
   let v=(evt.screenY-150)/150;
   if (v>0)
     v=1-v;
--- app/launch_detector.js
+++ app/launch_detector.js
@@ -1,32 +1,52 @@
 import { OrientationSensor } from "orientation";
 export class LaunchDetector{
-    constructor (onLaunchTriggered,timeout) {
+    constructor (onLaunchTriggered) {
         let me=this;
         me.accumulator=[];
+        me.accumulatorSum=0;
         me.size=10;
-        me.index=0;
+        me.index=-1;
         me.onLaunchTriggered=onLaunchTriggered;
-        me.orientation = new OrientationSensor({ frequency: 4 });
+        me.orientation = new OrientationSensor({ frequency: 10 });
         me.orientation.addEventListener("reading", ()=> {
             me.onOrientationChanged(me.orientation.quaternion);
         });
         me.calibrationMode=false;
         this.lastValue=null;
+        this.isStarted=false;
     }
         
-    start(){
+    start(threshold){
+        this.threshold=threshold??2;
+        this.accumulator=[];
+        this.accumulatorSum=0;
+        this.accumulatorSumMax=0;
+        this.index=-1;
+        this.isStarted=true;
         this.orientation.start();
     }
 
     stop(){
+        this.isStarted=false;
         this.orientation.stop();
         this.calibrationMode=false;
     }
 
-    calibrate(onCalibrated){
+    toggle(){
+        if (this.isStarted) this.stop();
+        else this.start();
+    }
+
+    calibrate(onCalibrating,onCalibrated){
+        let me=this;
         this.start();
         this.calibrationMode=true;
+        this.onCalibrating=onCalibrating;
         this.onCalibrated=onCalibrated;
+        setTimeout(() => {
+            me.threshold=me.accumulatorSumMax;
+            me.onCalibrated(me.accumulatorSumMax);
+        }, 5000);
     }
     setChart(chart){
         this.chart=chart;
@@ -44,41 +64,56 @@ export class LaunchDetector{
         //console.log(angle);
         this.accumulate(angle);
         
-        if (this.accumulator.length<this.size) return;
-        
         if (this.chart!=null) this.chart.update(this.accumulator);
         
-        let sum=0;
+        /*let sum=0;
         this.accumulator.forEach(element => {
             sum+=element;
-        });
-        console.log(sum);
-
-        if (Math.abs(sum)>2.5) this.onLaunchTriggered();
+        });*/
+        
+        //console.log(this.accumulatorSum);
+        if (this.calibrationMode){
+            this.accumulatorSumMax=Math.max(this.accumulatorSumMax,Math.abs(this.accumulatorSum));
+            this.onCalibrating(this.accumulatorSum);
+            //console.log(this.accumulatorSum);
+        }else{
+            if (Math.abs(this.accumulatorSum)>this.threshold) {
+                console.log("before on trigger threshold:"+ this.threshold);
+                this.onLaunchTriggered();
+            }
+        }
     }
 
     accumulate(value){
+        //console.warn("value: "+value,JSON.stringify(this.accumulator));
+
         if (!value) value=0;
         this.index++;
         if (this.index>=this.size) {
+            this.accumulatorSum-=this.accumulator[0];
             this.index=this.size-1;
-            this.accumulator=this.accumulator.slice(1,this.index);
+            this.accumulator=this.accumulator.slice(1,this.size);
         }
         if (this.lastValue==null)this.lastValue=value;
-        this.accumulator[this.index]=this.diff(value,this.lastValue);
+        
+        let diff=this.diff(value,this.lastValue);
+        this.accumulatorSum+=diff;
+
+        this.accumulator[this.index]=diff;
         this.lastValue=value;
         //console.log(value+" "+this.index+" "+ this.accumulator[this.index]);
-        //console.log(value,JSON.stringify(this.accumulator));
+        //console.log("value: "+value,JSON.stringify(this.accumulator));
     }
     
     diff(v1,v2){
-        let res=Math.abs(v1-v2);
-        if (res>1)
+        let res=(v1-v2);
+        if (Math.abs(res>=2))
         {
-            res=Math.abs(v1+v2);
-            //console.log("++++++++",v1,v2,res);
-        }// else
-            //console.log("--------",v1,v2,res);
+            //console.log("v1: "+v1+"v2: "+v2+" res: "+res);
+            res=(v1+v2);
+           // console.warn("v1: "+v1+"v2: "+v2+" res: "+res);
+        }
+        //console.error("v1: "+v1+"v2: "+v2+" res: "+res);
         return res;
     }
 
